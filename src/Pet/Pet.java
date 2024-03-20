cc5f2adcf3f4995d793fbbec1546f130c218ac00
gabriel_m_silva10 <gabriel_m_silva10@estudante.sesisenai.org.br>
2024-03-19 21:21:23 -0300

    Initial commit

--- src/Pet/Pet.java
+++ src/Pet/Pet.java
@@ -4,16 +4,21 @@ public class Pet {
     String nome;
     String especie;
     int idade;
-    //Métado Construtor
+    //Método Construtor
     //Cria um contrato de construção
-    public Pet(String nome, String especie, int idade){
+    public Pet(String nome,String especie,int idade){
         this.nome = nome;
         this.especie = especie;
         this.idade = idade;
     }
+    //construtor default ou padrão sem parametros
+    public Pet(){
+
+    }
+
     public void exibir(){
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Espécie: " + this.especie);
-        System.out.println("Idade: " + this.idade);
+        System.out.println("Nome:"+this.nome);
+        System.out.println("Espécie:"+this.especie);
+        System.out.println("Idade:"+this.idade);
     }
 }
