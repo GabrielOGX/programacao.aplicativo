0925f2088225ead70ab5a9440ed61523f1b1e683
gabriel_m_silva10 <gabriel_m_silva10@estudante.sesisenai.org.br>
2024-03-19 21:41:21 -0300

    Initial commit

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -4,6 +4,31 @@ public class Animal {
     String nome;
     String tipo;
     int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
     public void emitirSom(String som){
         System.out.println("O animal faz "+som);
     }
