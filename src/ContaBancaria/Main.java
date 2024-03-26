3f3bcb3625cfcf8a99e6ac079e06cd68e7171e26
gabriel_m_silva10 <gabriel_m_silva10@estudante.sesisenai.org.br>
2024-03-26 19:06:09 -0300

    Initial commit

---
+++ src/ContaBancaria/Main.java
@@ -0,0 +1,19 @@
+package ContaBancaria;
+
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            Scanner leia = new Scanner(System.in);
+            double valor;
+            //criar o objeto conta bancaria
+            ContaBancaria conta1 = new ContaBancaria();
+            System.out.println("Informe o valor:");
+            valor = leia.nextDouble();
+            conta1.depositar(valor);
+            conta1.exibirSaldo();
+            System.out.println("Digite o valor do saque:");
+            double saque = leia.nextDouble();
+            conta1.sacar(saque);
+            conta1.exibirSaldo();
+        }
+    }
